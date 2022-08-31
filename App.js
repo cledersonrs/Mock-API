5ac762306ea882d49ac670a46e71924744dbdd6c
cledersonrs <cledersonrs@gmail.com>
2022-08-31 09:21:09 -0300

    app mobile using mock API

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Importing screens
+import Clientes from './screens/Clientes';
+import Saldos from './screens/Saldos';
+
+//Importing navigation libraries 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
 export default function App() {
+  const Stack = createStackNavigator();
+
+  const Menu = () => {
+    return (
+      <Stack.Navigator initialRouteName='Clientes' screenOptions={() => ({ headerShown: false })}>
+        <Stack.Screen name='Clientes' component={Clientes} />
+        <Stack.Screen name='Saldos' component={Saldos} />
+      </Stack.Navigator>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Menu />
+    </NavigationContainer>
   );
 }
 
